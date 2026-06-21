b2598a2f8e73306c1195db3058ae67d18822a40e
kritarth2135 <kritarthvarma2135@gmail.com>
2026-06-21 22:08:04 +0530

    Partial implementation of create_cube function

--- rubiks_cube.c
+++ rubiks_cube.c
@@ -2,15 +2,27 @@
 
 #include "rubiks_cube.h"
 
-RubiksCube * create_cube(void) {
-    RubiksCube *cube = malloc(sizeof(RubiksCube));
+rubiks_cube * create_cube(void) {
+    rubiks_cube *cube = malloc(sizeof(rubiks_cube));
     if (cube == NULL) {
         return NULL;
     }
 
-    for (int i = 0; i < FACES_OF_CUBE; i++) {
-        cube->bitboards[i] = 0ULL;
-    }
+    for (int i = 0; i < NUMBER_OF_FACES; i++) {
+        face *temp = malloc(sizeof(face));
+        if (temp == NULL) {
+            for (int j = i - 1; j >= 0; j--) {
+                free(cube->faces[i]);
+            }
+            free(cube);
+            return NULL;
+        }
 
-    return cube;
+        temp->color = i;
+        for (int j = 0; i < FACE_DIMENSION; i++) {
+            for (int k = 0; k < FACE_DIMENSION; k++) {
+                temp->facets[j][k] = i;
+            }
+        }
+    }
 }
