3a40d524c1334183ea7da0357cef4cd5092bb473
kritarth2135 <kritarthvarma2135@gmail.com>
2026-06-21 22:06:51 +0530

    Removed everything from this file.

--- main.c
+++ main.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-
-#include "rubiks_cube.h"
-
 int main(void) {
-    RubiksCube *cube = create_cube();
-    if (cube == NULL) {
-        return 1;
-    }
-
-    for (int i = 0; i < FACES_OF_CUBE; i++) {
-        printf("%i", cube->bitboards[i]);
-    }
+    return 0;
 }
