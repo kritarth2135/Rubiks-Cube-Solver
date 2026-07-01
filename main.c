30fad12caa5840950af9bbdc44c4260b13137844
kritarth2135 <kritarthvarma2135@gmail.com>
2026-07-01 17:36:39 +0530

    Added loop for testing the cube

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
 // #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "rubiks_cube.h"
+
+#define BUFFER_SIZE 50
+
+void get_input(char buffer[BUFFER_SIZE]);
 
 int main(void) {
     RubiksCube *cube = create_rubiks_cube();
     if (cube == NULL) {
         return 1;
     }
+
+    char buffer[BUFFER_SIZE];
     print_cube(cube);
+    while (1) {
+        printf("Enter move ('q' to exit): ");
+        if (buffer[0] == 'q') {
+            break;
+        }
+        get_input(buffer);
+        make_move(cube, buffer);
+        print_cube(cube);
+    }
+
     free(cube);
     return 0;
+}
+
+void get_input(char buffer[BUFFER_SIZE]) {
+    int idx = 0;
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        buffer[idx] = ch;
+        idx++;
+    }
+    buffer[idx] = '\0';
 }
