15979b39fb9f55c24328a8bb7f0b2e42250cc8bd
kritarth2135 <kritarthvarma2135@gmail.com>
2026-07-07 22:05:23 +0530

    Switched to lehmer index for encoding

--- pattern_database.c
+++ pattern_database.c
@@ -1,37 +1,77 @@
 #include <stdint.h>
+#include <math.h>
 
 #include "rubiks_cube.h"
 #include "pattern_database.h"
 
-#define CORNER_POSITION_BITS 3
-#define CORNER_ORIENTATION_BITS 2
-#define EDGE_POSITION_BITS 4
-#define EDGE_ORIENTATION_BITS 1
+#define MAX_SIZE 12
+#define ALL_POSSIBLE_CORNER_ORIENTATIONS 2187  // 3^7
+#define ALL_POSSIBLE_EDGE_ORIENTATIONS 2048    // 2^11
 
-// Each corner is encoded into 5 bits, the first 3 bits indicating the position
-// of the corner in the cube and the other 2 bits indicating the orientation of
-// the corner.
-uint64_t encode_corners(RubiksCube *cube) {
-    uint64_t value = 0;
-    for (int i = 0; i < NUMBER_OF_CORNERS; i++) {
-        value <<= CORNER_POSITION_BITS;
-        value |= cube->corner_positions[i];
-        value <<= CORNER_ORIENTATION_BITS;
-        value |= cube->corner_orientations[i];
+// Precomputed factorials
+const int FACTORIAL[] = {
+    1,
+    1,
+    2,
+    6,
+    24,
+    120,
+    720,
+    5040,
+    40320,
+    362880,
+    3628800,
+    39916800,
+    479001600
+};
+
+int lehmer_idx(int array[], int size) {
+    if (size > MAX_SIZE) {
+        return -1;
+    }
+    int count_array[size];
+
+    for (int i = 0; i < size; i++) {
+        int count = 0;
+        for (int j = i + 1; j < size; j++) {
+            if (array[j] < array[i]) {
+                count++;
+            }
+        }
+        count_array[i] = count;
+    }
+
+    int idx = 0;
+    for (int i = 0; i < size; i++) {
+        idx += count_array[i] * FACTORIAL[size - 1 - i];
+    }
+    return idx;
+}
+
+int to_base_10(int array[], int size, int base) {
+    int value = 0;
+    for (int i = 0; i < size; i++) {
+        value = value * base + array[size - 1 - i];
     }
     return value;
 }
 
-// Each edge is encoded into 5 bits, the first 4 bits indicating the position
-// of the edge in the cube and the other 1 bit indicating the orientation of
-// the edge.
-uint64_t encode_edges(RubiksCube *cube) {
-    uint64_t value = 0;
+uint64_t encode_corners(RubiksCube *cube) {
+    int positions[NUMBER_OF_CORNERS];
+    int orientations[NUMBER_OF_CORNERS];
     for (int i = 0; i < NUMBER_OF_CORNERS; i++) {
-        value <<= EDGE_POSITION_BITS;
-        value |= cube->edge_positions[i];
-        value <<= EDGE_ORIENTATION_BITS;
-        value |= cube->edge_orientations[i];
+        positions[i] = cube->corner_positions[i];
+        orientations[i] = cube->corner_orientations[i];
     }
-    return value;
+    return (lehmer_idx(positions, NUMBER_OF_CORNERS) * ALL_POSSIBLE_CORNER_ORIENTATIONS) + to_base_10(orientations, NUMBER_OF_CORNERS - 1, CORNER_SIDES);
+}
+
+uint64_t encode_edges(RubiksCube *cube) {
+    int positions[NUMBER_OF_EDGES];
+    int orientations[NUMBER_OF_EDGES];
+    for (int i = 0; i < NUMBER_OF_EDGES; i++) {
+        positions[i] = cube->edge_positions[i];
+        orientations[i] = cube->edge_orientations[i];
+    }
+    return (lehmer_idx(positions, NUMBER_OF_EDGES) * ALL_POSSIBLE_EDGE_ORIENTATIONS) + to_base_10(orientations, NUMBER_OF_EDGES - 1, EDGE_SIDES);
 }
