f18a7fb1675a021ed659b3af0bc44547a33b3d8e
kritarth2135 <kritarthvarma2135@gmail.com>
2026-07-11 22:03:04 +0530

    Updated main for testing

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "rubiks_cube.h"
+#include "pattern_database.h"
 
 #define BUFFER_SIZE 50
 
@@ -13,8 +15,22 @@ int main(void) {
         return 1;
     }
 
+    // Loading databases
+    uint8_t *corner_db = load_db(MAX_DEPTH, BASIC_MOVES, REVERSE_BASIC_MOVES, NUMBER_OF_BASIC_MOVES, CORNER_DB_NAME, POSSIBLE_CORNER_COMBINATIONS);
+    uint8_t *first_six_edge_db = load_db(MAX_DEPTH, BASIC_MOVES, REVERSE_BASIC_MOVES, NUMBER_OF_BASIC_MOVES, FIRST_SIX_EDGE_DB_NAME, POSSIBLE_SIX_EDGE_COMBINATIONS);
+    uint8_t *last_six_edge_db = load_db(MAX_DEPTH, BASIC_MOVES, REVERSE_BASIC_MOVES, NUMBER_OF_BASIC_MOVES, LAST_SIX_EDGE_DB_NAME, POSSIBLE_SIX_EDGE_COMBINATIONS);
+    if (corner_db == NULL || first_six_edge_db == NULL || last_six_edge_db == NULL) {
+        return 1;
+    }
+    printf("\n");
+
     char buffer[BUFFER_SIZE];
+    uint64_t indexes[NUMBER_OF_INDEXES];
     print_cube(cube);
+    print_cube_arrays(cube);
+    encode_corners_and_edges(cube, indexes);
+    printf("[%lu, %lu, %lu, %lu]\n", encode_centres(cube), indexes[0], indexes[1], indexes[2]);
+
     while (1) {
         printf("Enter move ('q' to exit): ");
         get_input(buffer);
@@ -26,9 +42,15 @@ int main(void) {
         }
         printf("\n");
         print_cube(cube);
+        print_cube_arrays(cube);
+        encode_corners_and_edges(cube, indexes);
+        printf("[%lu, %lu, %lu, %lu]\n", encode_centres(cube), indexes[0], indexes[1], indexes[2]);
     }
 
     free(cube);
+    free(corner_db);
+    free(first_six_edge_db);
+    free(last_six_edge_db);
     return 0;
 }
 
